848ff940b62ed97f2f36363e28377b904574d51f
carlosray <carlos.ray@mail.ru>
2021-02-23 14:45:38 +0300

    basic auth

--- src/main/java/ru/vas/apigateway/config/SecurityConfiguration.java
+++ src/main/java/ru/vas/apigateway/config/SecurityConfiguration.java
@@ -1,14 +1,21 @@
 package ru.vas.apigateway.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.vas.apigateway.filter.CustomAuthenticationEntryPoint;
 import ru.vas.apigateway.filter.JwtTokenAuthenticationFilter;
 
 @Configuration
@@ -16,12 +23,20 @@ import ru.vas.apigateway.filter.JwtTokenAuthenticationFilter;
 @RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final JwtTokenAuthenticationFilter jwtTokenAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint basicAuthenticationEntryPoint;
+    @Value("${auth.basic.user}")
+    private String basicUser;
+    @Value("${auth.basic.pass}")
+    private String basicPass;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .httpBasic().disable()
+                .httpBasic()
+                .realmName("Checker")
+                .authenticationEntryPoint(basicAuthenticationEntryPoint)
+                .and()
 
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
@@ -34,10 +49,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth/register").permitAll()
                 .antMatchers("/auth/login").permitAll()
                 .antMatchers("/auth/logout").permitAll()
+                .antMatchers("/actuator/**").hasRole("BASIC")
                 .anyRequest().authenticated()
 
                 .and()
                 .addFilterBefore(jwtTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(basicUser).password(passwordEncoder().encode(basicPass))
+                .authorities("ROLE_BASIC");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
